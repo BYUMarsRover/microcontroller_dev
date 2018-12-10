ef739b73e8259f71ce079aa2d5456267a371cdd0
Zack Alger <zack@a7ger.com>
2018-12-10 14:46:17 -0700

    Many changes... untested
    no longer read/writing amps. error checking/handling implemented. reads digital pin for error state bool

--- onboard_mega/Wheels.h
+++ onboard_mega/Wheels.h
@@ -10,76 +10,71 @@ class Wheels {
 public:
 
   Wheels(){  
-    right_front_set_speed = 125;
+    right_front_set_speed = 35;
     right_front_dir = true;
     right_front_enable = true;
     right_front_actual_speed = 0;
-    right_front_amps = 0;
     right_front_error = false;
     
     right_middle_set_speed = 0;
     right_middle_dir = true;
     right_middle_enable = true;
     right_middle_actual_speed = 0;
-    right_middle_amps = 0;
     right_middle_error = false;
     
     right_rear_set_speed = 0;
     right_rear_dir = true;
     right_rear_enable = true;
     right_rear_actual_speed = 0;
-    right_rear_amps = 0;
     right_rear_error = false;
   
-    left_front_set_speed = 125;
+    left_front_set_speed = 35;
     left_front_dir = true;
     left_front_enable = true;
     left_front_actual_speed = 0;
-    left_front_amps = 0;
     left_front_error = false;
     
     left_middle_set_speed = 0;
     left_middle_dir = true;
     left_middle_enable = true;
     left_middle_actual_speed = 0;
-    left_middle_amps = 0;
     left_middle_error = false;
     
     left_rear_set_speed = 0;
     left_rear_dir = true;
     left_rear_enable = true;
     left_rear_actual_speed = 0;
-    left_rear_amps = 0;
     left_rear_error = false;
   }
   
   ~Wheels(){}
 
-  void determineEnableValues() {
-    if (left_front_set_speed < (255 * (MIN_SET_SPEED_PERCENTAGE / 100))) left_front_enable = false;
-    if (left_middle_set_speed < (255 * (MIN_SET_SPEED_PERCENTAGE / 100))) left_middle_enable = false;
-    if (left_rear_set_speed < (255 * (MIN_SET_SPEED_PERCENTAGE / 100))) left_rear_enable = false;
-    if (right_front_set_speed < (255 * (MIN_SET_SPEED_PERCENTAGE / 100))) right_front_enable = false;
-    if (right_middle_set_speed < (255 * (MIN_SET_SPEED_PERCENTAGE / 100))) right_middle_enable = false;
-    if (right_rear_set_speed < (255 * (MIN_SET_SPEED_PERCENTAGE / 100))) right_rear_enable = false;
+  void updateFeedbackData() {
+    
+    this->right_front_actual_speed = convertToRpm(analogRead(RIGHT_FRONT_WHEEL_ACTUAL_SPEED));
+    this->right_front_error = digitalRead(RIGHT_FRONT_WHEEL_ERROR);
+    
+    this->right_middle_actual_speed = convertToRpm(analogRead(RIGHT_MIDDLE_WHEEL_ACTUAL_SPEED));
+    this->right_middle_error = digitalRead(RIGHT_MIDDLE_WHEEL_ERROR);
+    
+    this->right_rear_actual_speed = convertToRpm(analogRead(RIGHT_REAR_WHEEL_ACTUAL_SPEED));
+    this->right_rear_error = digitalRead(RIGHT_REAR_WHEEL_ERROR);
+    
+    this->left_front_actual_speed = convertToRpm(analogRead(LEFT_FRONT_WHEEL_ACTUAL_SPEED));
+    this->left_front_error = digitalRead(LEFT_FRONT_WHEEL_ERROR);
+    
+    this->left_middle_actual_speed = convertToRpm(analogRead(LEFT_MIDDLE_WHEEL_ACTUAL_SPEED));
+    this->left_middle_error = digitalRead(LEFT_MIDDLE_WHEEL_ERROR);
+    
+    this->left_rear_actual_speed = convertToRpm(analogRead(LEFT_REAR_WHEEL_ACTUAL_SPEED));
+    this->left_rear_error = digitalRead(LEFT_REAR_WHEEL_ERROR);
+
   }
 
-  void updateFeedbackData() {
-    this->right_front_actual_speed = map(analogRead(RIGHT_FRONT_WHEEL_ACTUAL_SPEED),0,1023,-6000,9000);
-//    this->right_front_actual_speed = map(analogRead(RIGHT_FRONT_WHEEL_ACTUAL_SPEED),0,1023,0,255);
-    this->right_middle_actual_speed = map(analogRead(RIGHT_MIDDLE_WHEEL_ACTUAL_SPEED),0,1023,0,255);
-    this->right_rear_actual_speed = map(analogRead(RIGHT_REAR_WHEEL_ACTUAL_SPEED),0,1023,0,255);
-    this->left_front_actual_speed = map(analogRead(LEFT_FRONT_WHEEL_ACTUAL_SPEED),0,1023,0,255);
-    this->left_middle_actual_speed = map(analogRead(LEFT_MIDDLE_WHEEL_ACTUAL_SPEED),0,1023,0,255);
-    this->left_rear_actual_speed = map(analogRead(LEFT_REAR_WHEEL_ACTUAL_SPEED),0,1023,0,255);
-
-    this->right_front_amps = map(analogRead(RIGHT_FRONT_WHEEL_AMPS),0,1023,0,255);
-    this->right_middle_amps = map(analogRead(RIGHT_MIDDLE_WHEEL_AMPS),0,1023,0,255);
-    this->right_rear_amps = map(analogRead(RIGHT_REAR_WHEEL_AMPS),0,1023,0,255);
-    this->left_front_amps = map(analogRead(LEFT_FRONT_WHEEL_AMPS),0,1023,0,255);
-    this->left_middle_amps = map(analogRead(LEFT_MIDDLE_WHEEL_AMPS),0,1023,0,255);
-    this->left_middle_amps = map(analogRead(LEFT_MIDDLE_WHEEL_AMPS),0,1023,0,255);
-
+  double convertToRpm(double rawSpeed) {
+    // escon output ranges from 0-4V while MEGA input ranges from 0-5V and we want to convert a range [0,1023] to [ESCON_RPM_RANGE_MIN, ESCON_RPM_RANGE_MAX]. 818.4 is 4/5 of 1023.
+    // this equation is the equation of a line on a graph representing the output range on the y axis and the input range on th x axis. y = mx + b
+    return ((((ESCON_RPM_RANGE_MAX - ESCON_RPM_RANGE_MIN) / 818.4) * rawSpeed) + ESCON_RPM_RANGE_MAX); 
   }
 
   void writeParams() {
@@ -156,46 +151,75 @@ public:
     return result;
   }
 
+  bool clearErrorStates() {
+    bool writeWheelParams = false;
+    if (this->right_front_error) {
+      this->right_front_enable = false;
+      digitalWrite(RIGHT_FRONT_WHEEL_ENABLE, right_front_enable);
+      writeWheelParams = true;
+    }
+    if (this->right_middle_error) {
+      this->right_middle_enable = false;
+      digitalWrite(RIGHT_MIDDLE_WHEEL_ENABLE, right_middle_enable);
+      writeWheelParams = true;
+    }
+    if (this->right_rear_error) {
+      this->right_rear_enable = false;
+      digitalWrite(RIGHT_REAR_WHEEL_ENABLE, right_rear_enable);
+      writeWheelParams = true;
+    }
+    if (this->left_front_error) {
+      this->left_front_enable = false;
+      digitalWrite(LEFT_FRONT_WHEEL_ENABLE, left_front_enable);
+      writeWheelParams = true;
+    }
+    if (this->left_middle_error) {
+      this->left_middle_enable = false;
+      digitalWrite(LEFT_MIDDLE_WHEEL_ENABLE, left_middle_enable);
+      writeWheelParams = true;
+    }
+    if (this->left_rear_error) {
+      this->left_rear_enable = false;
+      digitalWrite(LEFT_REAR_WHEEL_ENABLE, left_rear_enable);
+      writeWheelParams = true;
+    }
+    return writeWheelParams;
+  }
+
   byte right_front_set_speed;
   bool right_front_dir;
   bool right_front_enable;
   byte right_front_actual_speed;
-  byte right_front_amps;
   bool right_front_error;
   
   byte right_middle_set_speed;
   bool right_middle_dir;
   bool right_middle_enable;
   byte right_middle_actual_speed;
-  byte right_middle_amps;
   bool right_middle_error;
   
   byte right_rear_set_speed;
   bool right_rear_dir;
   bool right_rear_enable;
   byte right_rear_actual_speed;
-  byte right_rear_amps;
   bool right_rear_error;
 
   byte left_front_set_speed;
   bool left_front_dir;
   bool left_front_enable;
   byte left_front_actual_speed;
-  byte left_front_amps;
   bool left_front_error;
   
   byte left_middle_set_speed;
   bool left_middle_dir;
   bool left_middle_enable;
   byte left_middle_actual_speed;
-  byte left_middle_amps;
   bool left_middle_error;
   
   byte left_rear_set_speed;
   bool left_rear_dir;
   bool left_rear_enable;
   byte left_rear_actual_speed;
-  byte left_rear_amps;
   bool left_rear_error;
   
 };
--- onboard_mega/onboard_mega.ino
+++ onboard_mega/onboard_mega.ino
@@ -18,41 +18,30 @@ void setup() {
 void loop() {
   wheels.updateFeedbackData();
   if (writeWheelParams) {
-//    wheels.determineEnableValues();
     wheels.writeParams();
     writeWheelParams = false;
   }
-  clearErrorStates();
-  delay(10);
-}
-
-void clearErrorStates() {
-  //todo!!
+  writeWheelParams = wheels.clearErrorStates();
+  delay(125);
 }
 
 void writeWheelFeedback() {
   Wire.write(wheels.right_front_actual_speed);
-  Wire.write(wheels.right_front_amps);
   Wire.write(wheels.right_front_error);
 
   Wire.write(wheels.right_middle_actual_speed);
-  Wire.write(wheels.right_middle_amps);
   Wire.write(wheels.right_middle_error);
 
   Wire.write(wheels.right_rear_actual_speed);
-  Wire.write(wheels.right_rear_amps);
   Wire.write(wheels.right_rear_error);
 
   Wire.write(wheels.left_front_actual_speed);
-  Wire.write(wheels.left_front_amps);
   Wire.write(wheels.left_front_error);
 
   Wire.write(wheels.left_middle_actual_speed);
-  Wire.write(wheels.left_middle_amps);
   Wire.write(wheels.left_middle_error);
 
   Wire.write(wheels.left_rear_actual_speed);
-  Wire.write(wheels.left_rear_amps);
   Wire.write(wheels.left_rear_error);
 }
 
@@ -104,70 +93,41 @@ void setArmParams() {
   
 }
 
-/*
- * #define RIGHT_FRONT_WHEEL 3
-#define RIGHT_FRONT_WHEEL_SET_SPEED 7
-#define RIGHT_FRONT_WHEEL_DIR 40
-#define RIGHT_FRONT_WHEEL_ENABLE 44
-#define RIGHT_FRONT_WHEEL_ACTUAL_SPEED A4
-#define RIGHT_FRONT_WHEEL_AMPS A6
- */
 
 void setPins() {
-  pinMode(RIGHT_FRONT_WHEEL_SET_SPEED, OUTPUT);
-  pinMode(RIGHT_FRONT_WHEEL_DIR, OUTPUT);
-  pinMode(RIGHT_FRONT_WHEEL_ENABLE, OUTPUT);
-  pinMode(RIGHT_FRONT_WHEEL_ACTUAL_SPEED, INPUT);
-  pinMode(RIGHT_FRONT_WHEEL_AMPS, INPUT);
+  pinMode(LEFT_FRONT_WHEEL_SET_SPEED, OUTPUT);
+  pinMode(LEFT_FRONT_WHEEL_DIR, OUTPUT);
+  pinMode(LEFT_FRONT_WHEEL_ENABLE, OUTPUT);
+  pinMode(LEFT_FRONT_WHEEL_ACTUAL_SPEED, INPUT);
+  pinMode(LEFT_FRONT_WHEEL_ERROR, INPUT);
+  
+  pinMode(LEFT_MIDDLE_WHEEL_SET_SPEED, OUTPUT);
+  pinMode(LEFT_MIDDLE_WHEEL_DIR, OUTPUT);
+  pinMode(LEFT_MIDDLE_WHEEL_ENABLE, OUTPUT);
+  pinMode(LEFT_MIDDLE_WHEEL_ACTUAL_SPEED, INPUT);
+  pinMode(LEFT_MIDDLE_WHEEL_ERROR, INPUT);
+  
+  pinMode(LEFT_REAR_WHEEL_SET_SPEED, OUTPUT);
+  pinMode(LEFT_REAR_WHEEL_DIR, OUTPUT);
+  pinMode(LEFT_REAR_WHEEL_ENABLE, OUTPUT);
+  pinMode(LEFT_REAR_WHEEL_ACTUAL_SPEED, INPUT);
+  pinMode(LEFT_REAR_WHEEL_ERROR, INPUT);
+  
+  pinMode(LEFT_FRONT_WHEEL_SET_SPEED, OUTPUT);
+  pinMode(LEFT_FRONT_WHEEL_DIR, OUTPUT);
+  pinMode(LEFT_FRONT_WHEEL_ENABLE, OUTPUT);
+  pinMode(LEFT_FRONT_WHEEL_ACTUAL_SPEED, INPUT);
+  pinMode(LEFT_FRONT_WHEEL_ERROR, INPUT);
+  
+  pinMode(LEFT_MIDDLE_WHEEL_SET_SPEED, OUTPUT);
+  pinMode(LEFT_MIDDLE_WHEEL_DIR, OUTPUT);
+  pinMode(LEFT_MIDDLE_WHEEL_ENABLE, OUTPUT);
+  pinMode(LEFT_MIDDLE_WHEEL_ACTUAL_SPEED, INPUT);
+  pinMode(LEFT_MIDDLE_WHEEL_ERROR, INPUT);
+  
+  pinMode(LEFT_REAR_WHEEL_SET_SPEED, OUTPUT);
+  pinMode(LEFT_REAR_WHEEL_DIR, OUTPUT);
+  pinMode(LEFT_REAR_WHEEL_ENABLE, OUTPUT);
+  pinMode(LEFT_REAR_WHEEL_ACTUAL_SPEED, INPUT);
+  pinMode(LEFT_REAR_WHEEL_ERROR, INPUT);
 }
-
-/*
- * #define I2C_ADDRESS 8
-#define NUM_WHEELS 6
-
-#define LEFT_FRONT_WHEEL 0
-#define LEFT_FRONT_WHEEL_SET_SPEED 5
-#define LEFT_FRONT_WHEEL_DIR 48 
-#define LEFT_FRONT_WHEEL_ENABLE 52
-#define LEFT_FRONT_WHEEL_ACTUAL_SPEED A2
-#define LEFT_FRONT_WHEEL_AMPS A0
-
-#define LEFT_MIDDLE_WHEEL 1
-#define LEFT_MIDDLE_WHEEL_SET_SPEED 23
-#define LEFT_MIDDLE_WHEEL_DIR 23
-#define LEFT_MIDDLE_WHEEL_ENABLE 23
-#define LEFT_MIDDLE_WHEEL_ACTUAL_SPEED 23
-#define LEFT_MIDDLE_WHEEL_AMPS 23
-
-#define LEFT_REAR_WHEEL 2
-#define LEFT_REAR_WHEEL_SET_SPEED 23
-#define LEFT_REAR_WHEEL_DIR 23
-#define LEFT_REAR_WHEEL_ENABLE 23
-#define LEFT_REAR_WHEEL_ACTUAL_SPEED 23
-#define LEFT_REAR_WHEEL_AMPS 23
-
-#define RIGHT_FRONT_WHEEL 3
-#define RIGHT_FRONT_WHEEL_SET_SPEED 7
-#define RIGHT_FRONT_WHEEL_DIR 40
-#define RIGHT_FRONT_WHEEL_ENABLE 44
-#define RIGHT_FRONT_WHEEL_ACTUAL_SPEED A4
-#define RIGHT_FRONT_WHEEL_AMPS A6
-
-#define RIGHT_MIDDLE_WHEEL 4
-#define RIGHT_MIDDLE_WHEEL_SET_SPEED 23
-#define RIGHT_MIDDLE_WHEEL_DIR 23
-#define RIGHT_MIDDLE_WHEEL_ENABLE 23
-#define RIGHT_MIDDLE_WHEEL_ACTUAL_SPEED 23
-#define RIGHT_MIDDLE_WHEEL_AMPS 23
-
-#define RIGHT_REAR_WHEEL 5
-#define RIGHT_REAR_WHEEL_SET_SPEED 23
-#define RIGHT_REAR_WHEEL_DIR 23
-#define RIGHT_REAR_WHEEL_ENABLE 23
-#define RIGHT_REAR_WHEEL_ACTUAL_SPEED 23
-#define RIGHT_REAR_WHEEL_AMPS 23
-
-#define MIN_SET_SPEED_PERCENTAGE 3
-
-#endif
- */
