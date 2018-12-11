dad2c9556e5b7da9fdb50eb07709a032b3c79e34
Zack Alger <zack@a7ger.com>
2018-12-11 16:33:17 -0700

    right and left wheels direction are opposite

--- onboard_mega/Wheel.h
+++ onboard_mega/Wheel.h
@@ -17,14 +17,17 @@ public:
     this->actual_speed = 0;
     this->dir = true;
     this->error = false;
+
+    this->is_right_side_wheel = true;
   }
   
-  void init(uint8_t set_speed_pin, uint8_t enable_pin, uint8_t dir_pin, uint8_t actual_speed_pin, uint8_t error_pin){
+  void init(uint8_t set_speed_pin, uint8_t enable_pin, uint8_t dir_pin, uint8_t actual_speed_pin, uint8_t error_pin, bool is_right_side_wheel){
     this->set_speed_pin = set_speed_pin;
     this->enable_pin = enable_pin;
     this->dir_pin = dir_pin;
     this->actual_speed_pin = actual_speed_pin;
     this->error_pin = error_pin;
+    this->is_right_side_wheel = is_right_side_wheel;
     
     digitalWrite(enable_pin, true);
   }
@@ -48,7 +51,7 @@ public:
 
   void writeParams() {
     analogWrite(set_speed_pin, map(set_speed,0,255,(255*.1),(255*.9)));
-    digitalWrite(dir_pin, dir);
+    digitalWrite(dir_pin, is_right_side_wheel == dir);
   }
 
   uint8_t set_speed_pin;
@@ -62,6 +65,8 @@ public:
   
   bool dir;
   bool error;
+
+  bool is_right_side_wheel;
 };
 
 
