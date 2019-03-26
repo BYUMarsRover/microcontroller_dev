c6cfa25d18b49c743e999ce2c32b9cf4e0b3edf2
fake <fake@fakemail.com>
2019-03-26 12:11:33 -0600

    lower res tuned

--- display_cams.py
+++ display_cams.py
@@ -6,23 +6,18 @@ import sys
 
 ORIG_WIDTH = 160
 ORIG_HEIGHT = 120
+#ORIG_WIDTH = 1920
+#ORIG_HEIGHT = 1080
 RESIZE_WIDTH = 640
 RESIZE_HEIGHT = 480
 
 captures = []
-
-#for i in range(1, len(sys.argv)):
-#    captures.append(cv2.VideoCapture(int(sys.argv[i])))
 
 for i in range(1, len(sys.argv)):
     temp = cv2.VideoCapture(int(sys.argv[i]))
     temp.set(cv2.CAP_PROP_FRAME_WIDTH, ORIG_WIDTH)
     temp.set(cv2.CAP_PROP_FRAME_HEIGHT, ORIG_HEIGHT)
     captures.append(temp)
-
-
-#cap1 = cv2.VideoCapture(int(sys.argv[1]))
-#cap2 = cv2.VideoCapture(int(sys.argv[2]))
 
 while(True):
  
@@ -31,18 +26,9 @@ while(True):
     for i in range(len(captures)):
         _, frame = captures[i].read()
 	frames.append(cv2.resize(frame, (RESIZE_WIDTH,RESIZE_HEIGHT)))
-    
-    # Capture frame-by-frame
-#    _, frame1 = cap1.read()
-#    _, frame2 = cap2.read()
 
     for i in range(len(frames)):
         cv2.imshow('cam ' + str(i) + ' ', frames[i])
-
-
-    # Display the resulting frame
-#    cv2.imshow('cam1',frame1)
-#    cv2.imshow('cam2',frame2)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
