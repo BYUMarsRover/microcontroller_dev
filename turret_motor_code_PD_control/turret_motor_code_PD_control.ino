b8667f7f45f5ec297aeacb8433047d38d54fa202
Tucker Wilkes <twilkes149@gmail.com>
2019-04-10 19:17:38 -0600

    implemented master sm

--- turret_motor_code_PD_control/turret_motor_code_PD_control.ino
+++ turret_motor_code_PD_control/turret_motor_code_PD_control.ino
@@ -6,7 +6,12 @@
 #define ARM_PREAMBLE 2
 #define SCIENCE_PREAMBLE 3
 
-static enum masterState{POSITION, VELOCITY} currState;//master state for position or velocity control
+#define MAX_TIME 2000
+#define PERIOD 50
+
+static enum masterState{WAIT, POSITION, VELOCITY} currState;//master state for position or velocity control
+long prevTime = 0;//used to kill the turret if we don't receive a command within x seconds
+long tickCount = 0;
 
 void setup() {
   Serial.begin (9600);          
@@ -14,13 +19,36 @@ void setup() {
   Wire.begin(TURRET_ADDRESS);
   Wire.onReceive(onReceive);
   pinMode(ARM_TURRET_FB, INPUT);
+  prevTime = millis();
 
   PD_init();
+  VEL_init();
+  masterSM_init();
 }  
   
 void loop() {
-  PD_tick();
-  delay(50);
+  if (millis() >= tickCount+PERIOD) {//tick the SM every <period> milli seconds
+    masterSM_tick();
+    tickCount = millis();
+  }
+}
+
+void masterSM_init() {
+  currState = WAIT;
+}
+
+void masterSM_tick() {
+  if (currState == POSITION) {
+    PD_tick();
+  }
+  else if (currState == VELOCITY) {
+    if (millis() < prevTime+MAX_TIME) {//only tick if we've received a message recently
+     VEL_tick(); 
+    }
+    else {//otherwise stop the turret
+      stop_turret();
+    }
+  }
 }
   
 void onReceive(int howMany) 
@@ -30,9 +58,12 @@ void onReceive(int howMany)
   if (preamble == ARM_PREAMBLE) {//read two more bytes for turret position high and low
     turret_high = Wire.read();
     turret_low = Wire.read();
+    currState = POSITION;
   }
   else if (preamble == SCIENCE_PREAMBLE) {//read one more byte for turret direction
     direction = Wire.read();
+    currState = VELOCITY;
+    prevTime = millis();//save the last time we got a message for turret velocity
   }
   else {//flush wire
     while(Wire.available()) {
